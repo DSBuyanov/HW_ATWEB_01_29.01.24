6250f6c07ceebd225b7c67dae8642a710e8e0cea
DSBuyanov <inquis87@gmail.com>
2024-01-29 10:17:44 +0300

    AT web add screenshot

--- src/test/java/org/max/home/YourTestClass.java
+++ src/test/java/org/max/home/YourTestClass.java
@@ -1,14 +1,18 @@
 package org.max.home;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class YourTestClass {
@@ -21,17 +25,18 @@ public class YourTestClass {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         // Инициализация экземпляра ChromeDriver
         ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--start-maximized");
+        chromeOptions.addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver(chromeOptions);
         // Настройка ожидания элементов
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
     }
 
     @Test
     public void yourTest() {
         // Ваши действия в тесте
-        login("GB202308cd04f17", "98628f7f2b");
-        addDummy("dummyName", "dummyLogin");
+        login("Tester1347", "11bdc518d8");
+        addDummy("Dummy", "Tester1347");
         // Дополнительные шаги теста, если необходимо
         // ...
         // Сохранение скриншота окна браузера
@@ -40,9 +45,27 @@ public class YourTestClass {
 
     @AfterAll
     public static void tearDown() {
+        String timestamp = String.valueOf(System.currentTimeMillis());
+        saveScreenshot("screenshot_" + timestamp + ".png");
         // Завершение работы драйвера после выполнения всех тестов
         if (driver != null) {
             driver.quit();
+        }
+
+    }
+
+    // Метод для сохранения скриншота
+    private static void saveScreenshot(String filename) {
+        try {
+            // Используем класс TakesScreenshot для создания скриншота
+            TakesScreenshot screenshot = (TakesScreenshot) driver;
+            // Получаем изображение
+            File sourceFile = screenshot.getScreenshotAs(OutputType.FILE);
+            // Копируем изображение в директорию resources
+            File destinationFile = new File("src/main/resources/" + filename);
+            FileUtils.copyFile(sourceFile, destinationFile);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -64,11 +87,42 @@ public class YourTestClass {
         // Добавьте явные ожидания, если необходимо дождаться каких-либо элементов после логина
     }
 
-    private static void addDummy(String dummyName, String dummyLogin) {
-        // Ваши действия по добавлению dummy
-        // ...
+    private static void addDummy(String dummyName, String dummyDescription) {
+        try {
+            // Ваши действия по добавлению dummy
+
+            // Нажатие на кнопку '+'
+            WebElement addButton = driver.findElement(By.id("create-btn"));
+            addButton.click();
+
+            // Явное ожидание после нажатия кнопки '+'
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("create-item")));
+
+            // Ввод значения в поле Title
+            WebElement titleInput = driver.findElement(By.xpath("//*[@type='text']"));
+            titleInput.sendKeys(dummyName);
+
+            // Ввод значения в поле Description
+            WebElement descriptionInput = driver.findElement(By.cssSelector("div.field textarea.mdc-text-field__input"));
+            descriptionInput.sendKeys(dummyDescription);
+
+            // Нажатие кнопки Save
+            WebElement saveButton = driver.findElement(By.cssSelector("button[type='submit'][form='create-item']"));
+            saveButton.click();
+
+            // Явное ожидание после нажатия кнопки Save
+            wait.until(ExpectedConditions.invisibilityOfElementLocated(By.id("create-item")));
+
+            // Дополнительные шаги теста, если необходимо
+            // ...
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    // Дополнительные методы теста, если необходимо
-    // ...
+    private static WebElement waitForElement(By locator) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        return wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+    }
 }
